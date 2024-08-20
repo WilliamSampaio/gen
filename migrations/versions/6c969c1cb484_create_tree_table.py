21a6c42a038c0406e7a2bc4bceb41c0dd2217f9f
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-08-20 02:00:24 -0400

    add migration

---
+++ migrations/versions/6c969c1cb484_create_tree_table.py
@@ -0,0 +1,41 @@
+"""create tree table
+
+Revision ID: 6c969c1cb484
+Revises:
+Create Date: 2024-08-20 01:03:18.769076
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.sql import func
+
+# revision identifiers, used by Alembic.
+revision: str = '6c969c1cb484'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'tree',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('fs_id', sa.String(8), nullable=False, unique=True),
+        sa.Column('name', sa.String(100), nullable=False),
+        sa.Column('born_in', sa.Date()),
+        sa.Column('died_in', sa.Date()),
+        sa.Column('gender', sa.CHAR()),
+        sa.Column('father_id', sa.Integer, sa.ForeignKey('tree.id')),
+        sa.Column('mother_id', sa.Integer, sa.ForeignKey('tree.id')),
+        sa.Column(
+            'inserted_in',
+            sa.DateTime(),
+            nullable=False,
+            server_default=func.now(),
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('tree')
