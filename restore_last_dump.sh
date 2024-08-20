95d432d240793a51282ffb189bbaac02170957cf
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-08-20 02:58:57 -0400

    restore dump

---
+++ restore_last_dump.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DUMP=$(find dumps -type f -exec ls -t1 {} + | head -1)
+
+echo $DUMP
+
+docker cp $DUMP gen-database:/last_dump
+docker exec gen-database pg_restore -U genuser -d postgres /last_dump
+docker exec gen-database rm /last_dump
