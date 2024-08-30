f3370076582101814e4f1ed194c86a08978da3c6
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-08-29 20:18:14 -0400

    update

--- gen/database/__init__.py
+++ gen/database/__init__.py
@@ -101,12 +101,11 @@ def get_roots():
 
 def get_leaves():
     sql = """
-    select distinct t.id from tree t
+    select distinct t.id, t.inserted_in from tree t
     where
-        t.leaf is false and
-        (	t.id not in (select fo.father_id from father_of fo) and
-            t.id not in (select mo.mother_id from mother_of mo)
-        )
-    order by t.id asc
+        t.leaf is false
+        and t.id not in (select fo.father_id from father_of fo)
+        and t.id not in (select mo.mother_id from mother_of mo)
+    order by t.inserted_in asc
     """
     return [x[0] for x in Session().execute(text(sql)).all()]
