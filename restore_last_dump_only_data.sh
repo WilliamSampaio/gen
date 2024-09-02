03bd30a13879b548882fa4b288049007443edca9
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-09-01 21:58:56 -0400

    update scripts

--- restore_last_dump_only_data.sh
+++ restore_last_dump_only_data.sh
@@ -8,5 +8,7 @@ docker cp $DUMP gen-database:/last_dump
 docker exec gen-database pg_restore \
     -U genuser \
     -d postgres \
+    --data-only \
+    --disable-triggers \
     /last_dump
 docker exec gen-database rm /last_dump
