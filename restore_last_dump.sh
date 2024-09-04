401e3e0eed2822e94dd275be81d114a37f2ffd75
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-09-03 23:02:07 -0400

    update

--- restore_last_dump.sh
+++ restore_last_dump.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-DUMP=$(find dumps -type f -exec ls -t1 {} + | head -1)
+if [[ -z $1 ]];
+then 
+    DUMP=$(find dumps -type f -exec ls -t1 {} + | head -1)
+else
+    DUMP="$1"
+fi
 
 echo $DUMP
+
+if [[ $2 == "-d" ]];
+then
+    DATA_ONLY="--data-only --disable-triggers "
+else
+    DATA_ONLY=""
+fi
 
 docker cp $DUMP gen-database:/last_dump
 docker exec gen-database pg_restore \
     -U genuser \
-    -d postgres \
+    -d postgres $DATA_ONLY\
     /last_dump
 docker exec gen-database rm /last_dump
